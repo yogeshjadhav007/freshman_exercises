c46b39efdd29ac0df6eca121c09b927877ec97cc
nikhil <nikhil.r.phadtare@gmail.com>
2023-03-21 15:31:43 +0530

    Two new exercises added.

--- function_exercises.py
+++ function_exercises.py
@@ -19,3 +19,15 @@
 # call nested function from within the main_function
 # Note: the nested and main functions defined above should have appropriate print statements.
 
+
+# define a function called main_function with argument "name" define another function named nested_function within the main_function which takes argument name also
+# nested_function should calculate the len of the name passed to it
+# print the length  of the string returned from the lenght function
+
+# The function defined above should handle exceptions
+# Example: None is passed instead of name in nested function
+# the nested function should raise an exception
+# the main function should catch that exception and handle it by printing the "Error"
+
+
+

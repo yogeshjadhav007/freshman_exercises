06b2fc3b851455b5e7c2b87c54d6b0c52e0ae225
nikhil <nikhil.r.phadtare@gmail.com>
2023-03-21 12:13:32 +0530

    commiting function tests

---
+++ function_exercises.py
@@ -0,0 +1,21 @@
+
+# Note: Write you code below the comments
+
+# Write a function that will take an argument and key word argument and print them
+
+
+# Write a function that will take in a number n as argument and and run a loop printing that the iteration number
+
+# Write a function that will take in an argument, if the argument is None, raise an exception
+
+# Write a function that will take in a kwargs as dictionary and print it.
+
+# Write a funcction that will compare length of two strings passed to it.
+
+# Write a function that will take in 2 arguments of type int ex. a and b, write nested loops where the loops should run
+# for a and b time resp.
+
+# define a function called main_function define another function named nested_function within the main_function
+# call nested function from within the main_function
+# Note: the nested and main functions defined above should have appropriate print statements.
+

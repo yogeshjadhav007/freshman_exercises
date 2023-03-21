a3c6e6deab730e8a5177b3266f36c4520b02a370
nikhil <nikhil.r.phadtare@gmail.com>
2023-03-21 18:38:37 +0530

    added documentation for the functions

--- advaned_functions.py
+++ advaned_functions.py
@@ -8,10 +8,18 @@ logger = Logger(__name__, 0)
 
 
 def calculate_harmonic_mean(arr: List) -> float:
+    '''
+        calculate the harmonic mean for the input array and return it.
+        do exception handling
+    '''
     raise NotImplemented()
 
 
 def print_directories(path: str):
+    '''
+        use os.listdir to print the directories in the path provided above.
+        handle exceptions
+    '''
     raise NotImplemented()
     
 
